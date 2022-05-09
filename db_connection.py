7b019c3c42847bdd4da0226668932c0718154220
Kevin Xu <kvnlxu@gmail.com>
2022-05-09 08:37:56 -0700

    Flagging for significant qoq and yoy changes (#1)

--- db_connection.py
+++ db_connection.py
@@ -16,6 +16,21 @@ class DBConnection:
         self.con = psycopg2.connect(**params)
         self.cur = self.con.cursor()
 
+    def update_quarter_counts(self, ticker, count):
+        insert_sql = '''
+            INSERT INTO quarter_counts (ticker, quarter_count)
+            VALUES (%s, %s)
+            ON CONFLICT (ticker) DO UPDATE SET
+            quarter_count = EXCLUDED.quarter_count;
+        '''
+        self.cur.execute(insert_sql, (ticker, count))
+
+    def get_quarter_counts(self):
+        query = 'SELECT ticker, quarter_count FROM quarter_counts'
+        self.cur.execute(query)
+        results = self.cur.fetchall()
+        return results
+
     def update_missing_metrics(self, ticker, metrics):
         insert_sql = '''
             INSERT INTO missing_metrics (ticker, metrics)
@@ -33,37 +48,54 @@ class DBConnection:
         self.cur.execute(delete_sql, (ticker,))
 
     def get_missing_metrics(self):
-        query = 'select * from missing_metrics'
+        query = 'SELECT ticker, metrics FROM missing_metrics'
         self.cur.execute(query)
         results = self.cur.fetchall()
         return results
 
-    def get_missing_metrics_dict(self):
-        query = 'select ticker, metrics from missing_metrics'
-        self.cur.execute(query)
-        results = self.cur.fetchall()
-        return dict(results)
-
-    def update_quarter_counts(self, ticker, count):
+    def update_qoq_percentages(self, ticker, metrics):
         insert_sql = '''
-            INSERT INTO quarter_counts (ticker, quarter_count)
+            INSERT INTO qoq_percentages (ticker, metrics)
             VALUES (%s, %s)
             ON CONFLICT (ticker) DO UPDATE SET
-            quarter_count = EXCLUDED.quarter_count;
+            metrics = EXCLUDED.metrics;
         '''
-        self.cur.execute(insert_sql, (ticker, count))
+        self.cur.execute(insert_sql, (ticker, str(metrics)))
 
-    def get_quarter_counts(self):
-        query = 'select * from quarter_counts'
+    def delete_qoq_percentages(self, ticker):
+        delete_sql = '''
+            DELETE FROM qoq_percentages
+            WHERE ticker=%s;
+        '''
+        self.cur.execute(delete_sql, (ticker,))
+
+    def get_qoq_percentages(self):
+        query = 'SELECT ticker, metrics FROM qoq_percentages'
         self.cur.execute(query)
         results = self.cur.fetchall()
         return results
 
-    def get_quarter_counts_dict(self):
-        query = 'select ticker, quarter_count from quarter_counts'
+    def update_yoy_percentages(self, ticker, metrics):
+        insert_sql = '''
+            INSERT INTO yoy_percentages (ticker, metrics)
+            VALUES (%s, %s)
+            ON CONFLICT (ticker) DO UPDATE SET
+            metrics = EXCLUDED.metrics;
+        '''
+        self.cur.execute(insert_sql, (ticker, str(metrics)))
+
+    def delete_yoy_percentages(self, ticker):
+        delete_sql = '''
+            DELETE FROM yoy_percentages
+            WHERE ticker=%s;
+        '''
+        self.cur.execute(delete_sql, (ticker,))
+
+    def get_yoy_percentages(self):
+        query = 'SELECT ticker, metrics FROM yoy_percentages'
         self.cur.execute(query)
         results = self.cur.fetchall()
-        return dict(results)
+        return results
 
     def close(self):
         self.con.commit()
